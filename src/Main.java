276e58c9661a1d8ad33cd19c7e28b720aebdd3b6
[Keegan Chapin] <[keeg.chapin@gmail.com]>
2023-09-12 13:43:34 -0400

    Cigs

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Keegan");
+        System.out.println("Hello friends");
+        System.out.println("Have a cigar");
     }
 }
