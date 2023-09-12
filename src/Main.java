296cb6b89ecc3f628fdd4fc613d970b9aee07ceb
[Keegan Chapin] <[keeg.chapin@gmail.com]>
2023-09-12 13:16:15 -0400

    input

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("What is you name");
 
-        System.out.println("Hello Keegan");
+        String userName = myObj.nextLine();
+
+        System.out.println("Hello " + userName);
     }
 }
